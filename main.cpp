2a9c33ce4835b0e6fa9532367a140ffad554e580
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-11-03 11:53:37 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,33 +3,33 @@
 #include <string>
 using namespace std;
 
-void insertion_sort(int *mas, unsigned int n){  
-   for(int i=0; i < n; i++){
-       for(int j=i; j>0 ; j--){
-           if(mas[j-1]>mas [j]){
-           swap (mas[j-1], mas[j]);
-           }
-           else break;
-           }
-       }
+void insertion_sort(int *mas, unsigned int n){
+    for(int i=0; i < n; i++){
+        for(int j=i; j>0 ; j--){
+            if(mas[j-1]>mas [j]){
+                swap (mas[j-1], mas[j]);
+            }
+            else break;
+        }
+    }
 }
 
 int main()
 {
-  unsigned int n;                            
+    unsigned int n;
     cin>> n;
-    cin.get();   
-    
+    cin.get();
+
     int *mas = new int [n];
     string stroka;
- getline (cin, stroka);
+    getline (cin, stroka);
     istringstream stream (stroka);
     for (unsigned int i=0; i<n;i++){
         if(!(stream>> mas[i])){
             cout<<"ERROR"<<endl;
             return -1;
         }
-    } 
+    }
     insertion_sort (mas,n);
     for(unsigned int i=0; i<n; i++){
         cout << mas[i] << " ";
